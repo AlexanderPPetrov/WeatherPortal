b4e6f5e6d4a314b19b3a567880244bbeb4e2b455
alexanderpetrov <alexander.petrov@novarto.com>
2016-07-25 16:57:19 +0300

    register form added

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,43 @@
-function initLoginPage(){
+function bindEvents(){
 
-    // Login button click handling
-    $('#login').on('click', function(e){
-        var valid = true,
-            username = $('#username').val(),
-            password = $('#password').val();
+    // Bind handlers for click events
+    $('#login').on('click', loginClick);
+    $('#register').on('click', registerClick);
 
-
-        if(username.length == 0){
-            $('#username').parent().find('.help-block').show();
-            valid = false;
-        }else{
-            $('#username').parent().find('.help-block').hide();
-        }
-
-        if(password.length == 0){
-            $('#password').parent().find('.help-block').show();
-            e.preventDefault();
-            valid = false;
-        }else{
-            $('#password').parent().find('.help-block').hide();
-        }
-
-        if(valid){
-            console.log('submit form')
-        }else{
-            // Do not submit form
-            e.preventDefault();
-        }
-    });
 }
 
+function loginClick (e){
+    var valid = true,
+        username = $('#username').val(),
+        password = $('#password').val();
+
+
+    if(username.length == 0){
+        $('#username').parent().find('.help-block').show();
+        valid = false;
+    }else{
+        $('#username').parent().find('.help-block').hide();
+    }
+
+    if(password.length == 0){
+        $('#password').parent().find('.help-block').show();
+        e.preventDefault();
+        valid = false;
+    }else{
+        $('#password').parent().find('.help-block').hide();
+    }
+
+    if(valid){
+        console.log('submit form')
+    }else{
+        // Do not submit form
+        e.preventDefault();
+    }
+}
+
+
+function registerClick(e){
+
+    $('#register-modal').modal('show');
+
+}
